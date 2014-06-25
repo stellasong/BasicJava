621309ecb3774cc0b8c56d01ee26675399e3f7ab
stellasong <yucanzhang2014@gmail.com>
2014-06-25 11:41:53 -0400

    Update Forecast.R

--- Forecast.R
+++ Forecast.R
@@ -1,10 +1,38 @@
-library("forecast")
 
-setwd("C:/Users/leo/Desktop/stat/DS training/Reproducible Research")
-dataSet <- read.table("./repdata_data_StormData.csv.bz2", header = TRUE, sep = ",")
+library("forecast");
 
-timeSeries <- ts(dataSet$Sold, frequency = 12)
+# trainingDataPath injected from Java
+data <- read.table(trainingDataPath, header=TRUE);
+#data <- read.table(file.choose(), header=TRUE);
 
-predictedValue <- forecast(ets(timeSeries), 1)
+#data$ID <- as.factor(data$ID);
+group <- split(data, data$ID);
 
-plot(predictedValue)
+#rDataPath <- "rdata.txt";
+if (file.exists(rDataPath)) file.remove(rDataPath);
+
+for(i in 1:length(group)){
+	
+  eachItem = group[[i]];
+  timeSeries <- ts(eachItem$Sold, frequency = 12)
+  #predictedValue <- forecast(tslm(timeSeries ~ trend + season), h=1);
+  predictedValue <- forecast(ets(timeSeries), 1);
+  write.table(predictedValue, rDataPath, append=TRUE, row.names = FALSE, col.names = FALSE);
+}
+
+
+#preds <- sapply(group, function(data) forecast(tslm(ts(data$Sold, frequency=12), 1)));
+# the model for this step is automatically chosen
+#timeSeries <- ts(data$Used, frequency = 12)
+
+# when the data has up-down trend /\_/\, can use ets (Exponential smoothing state space model)
+#predictedValue <- forecast(ets(timeSeries), 1)
+
+# to predict linear models, especially those involving trend and seasonality components.
+#predictedValue <- forecast(tslm(timeSeries ~ trend + season), h=1)
+
+# ARIMA (integrated autoregressive moving average model) for non-stationary time-series
+#predictedValue <- forecast(arima(timeSeries), h=1)
+
+#plot(predictedValue)
+#write.table(predictedValue, "rdata.txt", sep=";")
